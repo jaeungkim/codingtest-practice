d5bef4b9facae160d082856aac740cc34602532e
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2025-03-04 08:39:10 +0900

    [level 2] Title: 구명보트, Time: 35.37 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,25 @@
+function solution(people, limit) {
+    let answer=0;
+    people.sort((a,b) => a-b);
+    
+    // 50,50,70,80
+    let left=0;
+    let right=people.length-1;
+    
+    while(left<=right){
+        if(people[left]+people[right] <= limit){
+left++;
+            right--;
+}else{
+right--;
+}
+        answer++;
+        
+        
+}
+    
+        
+        
+        
+    return answer;
+}
